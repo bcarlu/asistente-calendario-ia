a287491f0df22fd118191b67ac80c7ca4d95f7c3
Brian Cardona <brian@localhost.localdomain>
2024-10-03 17:26:18 -0500

    se agrega ruta para el chat, se crea middleware de autenticacion y se actualiza ruta consultar-agenda

--- routes/aiRoutes.js
+++ routes/aiRoutes.js
@@ -1,56 +1,34 @@
 import { Router } from 'express';
 import { generarRespuestaIA } from '../controllers/aiController.js';
 import { obtenerHorasDisponibles, crearEvento, consultarAgenda } from '../controllers/aiController.js';
-import { google } from 'googleapis';
-import fs from 'fs';
 
 const router = Router();
 
-const OAuth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_CAL_CLIENT_ID,
-  process.env.GOOGLE_CAL_CLIENT_SECRET,
-  process.env.GOOGLE_CAL_REDIRECT_URI
-);
-
-// Ruta para conectar calendario de Google Calendar
-router.get('/conectar-calendario', (req, res) => {
-  // Generate the Google authentication URL
-  const url = OAuth2Client.generateAuthUrl({
-    access_type: 'offline', // Request offline access to receive a refresh token
-    scope:[
-      'https://www.googleapis.com/auth/calendar', // Scope for read-only access to the calendar
-      'https://www.googleapis.com/auth/userinfo.email' // Scope for access to the user's email
-    ]
+router.get('/chat', ensureAuthenticated, async (req, res) => {
+  res.render('chat', { 
+    title: 'Chat con Botpress', 
+    idUsuario: req.session.user.google_id, 
+    idCalendario: req.session.user.email 
   });
-  // Redirect the user to Google's OAuth 2.0 server
-  res.redirect(url);
 });
 
-// Ruta para recibir y guardar el token de Google Calendar
-router.get('/auth', async (req, res) => {
-  const code = req.query.code;
-
-  try {
-    const { tokens } = await OAuth2Client.getToken(code);
-    OAuth2Client.setCredentials(tokens);   
-
-    // Guardamos el refreshToken en un archivo (ajusta la ruta según tus necesidades)
-    fs.writeFileSync('token.json', JSON.stringify(tokens));
-    console.log('Token de actualización guardado correctamente');
-
-    res.send('Autenticado correctamente');
-  } catch (error) {
-    console.error('Error al obtener el token:', error);
-    res.status(500).send('Error de autenticación');
+// Middleware para verificar la autenticación
+function ensureAuthenticated(req, res, next) {
+  if (req.session.user) {
+    return next();
   }
-});
+  res.redirect('/login');
+}
 
 // Para consultar la disponibilidad en la agenda y responder mediante IA
 router.post('/consultar-agenda', async (req, res) => {
   const mensajeUsuario = req.body.mensaje;
+  const idUsuario = req.body.idUsuario;
+  const idCalendario = req.body.idCalendario
+  console.log("google id es:", idUsuario)
   
   try {
-    const respuesta = await generarRespuestaIA(mensajeUsuario);
+    const respuesta = await generarRespuestaIA(mensajeUsuario, idUsuario, idCalendario);
     res.json(respuesta);
   } catch (error) {
     res.status(500).json({ error: error.message });
